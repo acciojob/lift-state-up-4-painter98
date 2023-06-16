b58e5bd1e195129e11d06004677105b071aede35
painter98 <emailmepallavi@gmail.com>
2023-06-16 11:28:55 +0530

    change2

--- src/components/App.js
+++ src/components/App.js
@@ -37,12 +37,12 @@ const App = () => {
         {/* Do not remove the main div */}
         <h1>Parent Component</h1>
         <form onSubmit={(e)=>items(e)}>
-            Item Name<input type="text" className='itemName' onChange={(e)=>setName(e.target.value)}/>
+            Item Name<input type="text" className="itemName" onChange={(e)=>setName(e.target.value)}/>
             Item Price<input type="number" className="itemPrice" onChange={(e)=>setPrice(e.target.value)}/>
             <button>Add Items</button>
         </form>
+        <h2>Child Component</h2>
         <ul className="child">
-          <h2>Child Component</h2>
             {
                 cartItems.map((item,idx)=>{
                     return <Cart idx={idx} item={item} ondelete={deleting}/>
